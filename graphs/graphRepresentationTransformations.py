f504b9bbc20edcf3dad3e46728340406c8f99753
nikasakana <nikasakana@gmail.com>
2022-08-16 13:44:44 +0400

    update

---
+++ graphs/graphRepresentationTransformations.py
@@ -0,0 +1,17 @@
+#   There are 3 common graph representation structures:
+#       - Edge List:
+#           description: all pairs here represent edges in the graph for vertices with specific identifiers.
+#           example: [(0,1), (0,2), (2,3), (1,4)]
+#           pros: good for lookups of an edge or listing of all edges
+#           cons: bad for most common usages like find all neighbour vertices ( it will cost O(N) here )
+#       - Adjacency list:
+#           description: for vertex at index i list[i] represent all it's adjacent vertices ( neighbours )
+#           example: [[0,1,2], [3,4], [1]]
+#           pros: constant time lookup for adjacent vertices, which is needed for most of path finding algorithms
+#           cons: slow for edge operations
+#       - Adjacency matrix:
+#           description:  quadratic matrix of 0,1-s representing matrix[i][j]=1 as them being adjacent ( neighbours )
+#           example: [[1,0,0],[0,0,1], [0, 1, 0]]
+#           pros: good for edge lookups since we have constant time lookup for an edge.
+#           cons: more space needed than other representations. Slow for listing all neighbours of some vertex
+#
