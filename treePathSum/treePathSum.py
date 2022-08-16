f5e3dab3dfe6f49b023430e2826c4671e59eb61a
nikasakana <nikasakana@gmail.com>
2022-08-16 12:48:36 +0400

    added tree path sum algorithm

---
+++ treePathSum/treePathSum.py
@@ -0,0 +1,46 @@
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+
+
+class Solution:
+    def _recursiveHelper(self, root, targetSum, currentSum, currentList, resultLists):
+        if root is None:
+            return
+        elif (not root.left and not root.right):
+            if currentSum + root.val == targetSum:
+                currentList.append(root.val)
+                resultLists.append(currentList)
+        else:
+            if root.left:
+                copy1 = currentList[:]
+                copy1.append(root.val)
+                self._recursiveHelper(root.left, targetSum, currentSum + root.val, copy1, resultLists)
+            if root.right:
+                copy2 = currentList[:]
+                copy2.append(root.val)
+                self._recursiveHelper(root.right, targetSum, currentSum + root.val, copy2, resultLists)
+
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        result = []
+        self._recursiveHelper(root, targetSum, 0, [], result)
+        return result
+
+
+t1 = TreeNode(1)
+t2 = TreeNode(4)
+t3 = TreeNode(3, t1, t2)
+t4 = TreeNode(5)
+t5 = TreeNode(7)
+t6 = TreeNode(2, t4, t5)
+t7 = TreeNode(-6, t3, t6)
+
+solution = Solution()
+result = solution.pathSum(t7, 1)
+print(result)
+assert len(result) == 2
